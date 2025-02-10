a9772e766fae8224ba0714defc0734e57f23723f
setiyadiawal <setiyadiawal17@gmail.com>
2025-02-11 00:25:07 +0900

    Finishing

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ const tasksTodo = document.getElementById('tasksTodo')
 const tasksDone = document.getElementById('tasksDone')
 const tasksLate = document.getElementById('tasksLate')
 
-
-
 // Membuat value dari tanggal menjadi default hari ini
 window.addEventListener('DOMContentLoaded', dateLoad())
 function dateLoad () {
@@ -24,6 +22,7 @@ resetButton.addEventListener('click',() => {
 
 deleteAllTask.addEventListener("click", deleteAll)
 
+// Function menambahkan Task
 function addTask () {
     const taskText = inputText.value.trim()
     const priorityText = inputPriority.value
@@ -100,7 +99,7 @@ function renderTask(newTaskList) {
     editBtn.style.fontSize = '0.9em'
     editBtn.style.height = '1.8em'
     editBtn.addEventListener('click', () => {
-
+        editTask(liList, newTaskList)
     })
     
     const deleteBtn = document.createElement('button')
@@ -119,9 +118,8 @@ function renderTask(newTaskList) {
     // Masukkan value yang di input ke dalam li List
     textContainer.appendChild(checkbox)
     textContainer.appendChild(textSpan)
+
     liList.appendChild(textContainer)
-    // liList.appendChild(checkbox)
-    // liList.appendChild(textSpan)
     liList.appendChild(br)
     liList.appendChild(dateSpan)
     liList.appendChild(prioritySpan)
@@ -130,7 +128,15 @@ function renderTask(newTaskList) {
     // Masukkan semua yang ada pada li list ke dalam element ul todo
     ulTodo.appendChild(liList)
 
+    // Memasukkan ke dalam tasks late
+    const currentDate = new Date().toISOString().split("T")[0];
 
+    if ( inputDate.value < currentDate) {
+        ulLate.appendChild(liList)
+        checkbox.style.display = 'none'
+        textSpan.style.color = 'red'
+    }
+ 
     // Delete semua teks di kotak input
     inputContainer.reset()
 
@@ -138,6 +144,7 @@ function renderTask(newTaskList) {
     dateLoad()
 }
 
+// Function menghapus semua tasks
 function deleteAll () {
     ulTodo.remove()
     ulDone.remove()
@@ -153,4 +160,102 @@ function deleteAll () {
     tasksTodo.appendChild(newUltodo)
     tasksDone.appendChild(newUldone)
     tasksLate.appendChild(newUllate)
+}
+
+// Function edit task
+function editTask (liElement, task) {
+    liElement.querySelectorAll('div').forEach((div) => {
+        div.style.display = 'none'
+    });    
+    liElement.querySelectorAll('span').forEach((span) => {
+        span.style.display = 'none'
+    });    
+    
+    const editContainer = document.createElement('div')
+    editContainer.style.display = 'flex'
+    editContainer.style.flexDirection = 'column'
+    editContainer.style.gap = '5px'
+
+    const editInput = document.createElement('div')
+    editInput.style.display = 'flex'
+    editInput.style.gap = '5px'
+
+    const editText = document.createElement('input')
+    editText.type = 'text'
+    editText.value = task.text
+
+    const editPriority = document.createElement('select')
+    const priorityOptions = ['High Priority', 'Medium Priority', 'Low Priority']
+    priorityOptions.forEach((Option) =>{
+        const optionElement = document.createElement('option')
+        optionElement.value = Option
+        optionElement.textContent = Option
+        if (Option === task.priority) {
+            optionElement.selected = true
+        }
+        editPriority.appendChild(optionElement)
+    })
+
+    const editDate = document.createElement('input')
+    editDate.type = 'date'
+    editDate.value = task.date
+    editDate.style.fontSize = '1.2em'
+
+    const buttonContainer = document.createElement('div')
+    buttonContainer.style.display = 'flex'
+    buttonContainer.style.justifyContent = 'flex-end'
+    buttonContainer.style.width = '100%'
+    buttonContainer.style.gap = '5px'
+
+
+    const saveButton = document.createElement('button')
+    saveButton.type = 'submit'
+    saveButton.textContent = 'Save'
+    saveButton.className = 'addButton'
+    saveButton.style.fontSize = '0.9em'
+    saveButton.style.height = '1.8em'
+
+    const cancelButton = document.createElement('button')
+    cancelButton.type = 'button'
+    cancelButton.textContent = 'Cancel'
+    cancelButton.className = 'resetButton'
+    cancelButton.style.fontSize = '0.9em'
+    cancelButton.style.height = '1.8em'
+
+    editInput.appendChild(editText)
+    editInput.appendChild(editPriority)
+    editInput.appendChild(editDate)
+
+    buttonContainer.appendChild(saveButton)
+    buttonContainer.appendChild(cancelButton)
+    
+    editContainer.appendChild(editInput)
+    editContainer.appendChild(buttonContainer)
+
+    liElement.appendChild(editContainer)
+
+    // Save Button
+    saveButton.addEventListener('click', () => {
+        const updateTask = {
+            text: editText.value,
+            priority: editPriority.value,
+            date: editDate.value,
+            done: false
+        }
+
+        renderTask(updateTask)
+        liElement.remove()
+    })
+
+    // Close Button
+    cancelButton.addEventListener(('click'), () => {
+        editContainer.remove()
+
+        liElement.querySelectorAll('div').forEach((div) => {
+            div.style.display = 'flex'
+        });    
+        liElement.querySelectorAll('span').forEach((span) => {
+            span.style.display = 'inline'
+        });    
+    })
 }
